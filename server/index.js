68d42d8ad9e82ac790c631e36684fb97b4cf2535
Angelia Campbell <ali.belclamp@gmail.com>
2019-09-25 18:52:43 -0400

    include middleware for proxy to reroute requests to each service's api

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
+const newrelic = require('newrelic');
 const express = require('express');
 const app = express();
+const proxy = require('http-proxy-middleware')
 const port = 3030;
 
-
 app.use(express.static('./client'));
+
+var nearbyProxy = proxy('/api/', {target: 'http://localhost:3000/'});
+var descriptionProxy = proxy('/movies/', {target: 'http://localhost:4000/'});
+app.use(nearbyProxy);
+app.use(descriptionProxy);
+var reviewsProxy = proxy('/', {target: 'http://localhost:5000/'});
+app.use(reviewsProxy);
+
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
 });
